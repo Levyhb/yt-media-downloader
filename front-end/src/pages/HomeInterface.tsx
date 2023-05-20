5c36a15aa563289fa10448f104f3a842effcad0c
Levy Bezerra <levybholanda@gmail.com>
2023-05-20 10:20:35 -0300

    refactor: prettier lint

--- front-end/src/pages/HomeInterface.tsx
+++ front-end/src/pages/HomeInterface.tsx
@@ -1,46 +1,54 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function HomeInterface() {
-  const [url, setUrl] = useState('');
-  
+  const [url, setUrl] = useState("");
 
   const handleDownload = async (type: string) => {
     if (url) {
       try {
-        const response = type === 'video' ? await fetch(`http://127.0.0.1:8000/api/download-video?url=${url}`): await fetch(`http://127.0.0.1:8000/api/download-audio?url=${url}`);
+        const response =
+          type === "video"
+            ? await fetch(`http://127.0.0.1:8000/api/download-video?url=${url}`)
+            : await fetch(
+                `http://127.0.0.1:8000/api/download-audio?url=${url}`
+              );
         if (response.ok) {
           const blob = await response.blob();
           console.log(blob);
           const url = URL.createObjectURL(blob);
-          const a = document.createElement('a');
+          const a = document.createElement("a");
           a.href = url;
-          a.download =  type === 'video' ? 'video.mp4' : 'audio.mp3';
+          a.download = type === "video" ? "video.mp4" : "audio.mp3";
           a.click();
         } else {
-          console.log('Erro na solicitação');
+          console.log("Erro na solicitação");
         }
       } catch (error) {
-        console.log('Erro na solicitação', error);
+        console.log("Erro na solicitação", error);
       }
     } else {
-      console.log('URL inválida');
+      console.log("URL inválida");
     }
   };
 
   return (
     <div className="video-downloader-container">
-    <div>
-      <input type='text' placeholder='Video Url' onChange={(e) => setUrl(e.target.value)}/>
+      <div>
+        <input
+          type="text"
+          placeholder="Video Url"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="button" onClick={() => handleDownload("video")}>
+          Download Vídeo
+        </button>
+        <button type="button" onClick={() => handleDownload("audio")}>
+          Download Audio
+        </button>
+      </div>
     </div>
-    <div>
-      <button type='button' onClick={() => handleDownload('video')}>
-        Download Vídeo
-      </button>
-      <button type='button' onClick={() => handleDownload('audio')}>
-        Download Audio
-      </button>
-    </div>
-  </div>
   );
 }
 
