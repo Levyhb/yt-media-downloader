6a296eeef372f4f471196e52ac488a684bcc1b38
Levy Bezerra <levybholanda@gmail.com>
2023-05-25 12:30:15 -0300

    fix: production settings

--- yt-downloader-api/video_downloader_api/video_downloader_api/settings.py
+++ yt-downloader-api/video_downloader_api/video_downloader_api/settings.py
@@ -9,23 +9,34 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from dotenv import load_dotenv
+import os
+
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6f-9a+vco%c8hr2^=^_$%@u302u!azixru3%b&czw59av_62$-"
+SECRET_KEY = os.environ.get('SECRET_KEY')
+EXTERNAL_HOST = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://yt-media-downloader.vercel.app/',
+    'http://localhost:3000/'
+]
 
 
 # Application definition
