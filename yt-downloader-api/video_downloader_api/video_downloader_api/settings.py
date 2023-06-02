1cc8f6f764aadf5439fc25ed6d2e233484d071a7
Levy Bezerra <levybholanda@gmail.com>
2023-06-02 10:13:42 -0300

    Update settings.py

--- yt-downloader-api/video_downloader_api/video_downloader_api/settings.py
+++ yt-downloader-api/video_downloader_api/video_downloader_api/settings.py
@@ -137,9 +137,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = [
     'https://yt-media-downloader-api-production.up.railway.app',
-    'http://localhost:3000'
 ]
